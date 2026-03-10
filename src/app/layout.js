02db50c37cd62d6e20f2848ea8e58e544de4e82b
McWooden <garryf7887@gmail.com>
2026-03-10 22:21:00 +0700

    setup netlify indentity

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,8 +8,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <head>
+                <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+            </head>
             <body className="antialiased">
                 {children}
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+              if (window.netlifyIdentity) {
+                window.netlifyIdentity.on("init", user => {
+                  if (!user) {
+                    window.netlifyIdentity.on("login", () => {
+                      document.location.href = "/admin/";
+                    });
+                  }
+                });
+              }
+            `,
+                    }}
+                />
             </body>
         </html>
     );
